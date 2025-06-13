96d271e75f193a37062abe9b5565242e475d6004
EmperorKnight <jlpicado@uamv.edu.ni>
2025-06-13 16:29:20 -0600

    Modificado

--- Miercoles/Modelos/clase.py
+++ Miercoles/Modelos/clase.py
@@ -3,6 +3,6 @@ class productos:
         self.nombre = nombre
         self.precio = precio
         self.cantidad = cantidad
-    
+
     def __str__(self):
         return f"Producto: {self.nombre} \nPrecio: {self.precio} \nExistencia: {self.cantidad}"
